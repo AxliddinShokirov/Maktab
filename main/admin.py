212613a43de7d6ff2e7ec1f827e9a474877b982a
Axliddin <axliddins34@gmail.com>
2024-12-05 18:58:08 +0500

    a

---
+++ main/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from.models import *
+
+admin.site.register(Banner)
+admin.site.register(Course)
+admin.site.register(Contact)
+admin.site.register(Teacher)
+admin.site.register(Subject)
+admin.site.register(News)
+admin.site.register(CourseType)
+admin.site.register(Module)
+admin.site.register(Lesson)
+admin.site.register(Dimm)
+
